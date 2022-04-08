95080a9d39a0cb060a09bd59cca43ee0affa2e0f
discretetimefourier <44666921+rokasta12@users.noreply.github.com>
2022-04-08 18:46:52 +0300

    add: bloglist component

---
+++ src/common/constants/dummyData.ts
@@ -0,0 +1,9 @@
+export const dummyBlogs = [
+  {
+    title: "Deno , Node.js on steroids",
+    date: "12.04.2022",
+    status: "draft",
+    tags: ["javascript", "node.js"],
+    category: "Development",
+  },
+];
---
+++ src/components/BlogList/BlogTag.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  text: string;
+}
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="blog-tag base-body-12">
+    {{ text }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.blog-tag {
+  @apply px-[12px] py-[2px] inline-block  rounded-[12px] bg-yellow-400 ml-[2px];
+}
+</style>
