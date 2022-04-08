23218bfd8c4d34631a08fdd830f54b9348372575
muti.cansel <cancel.muti@gmail.com>
2022-04-09 01:39:38 +0300

    Merge branch 'main' of https://github.com/muticansel/commencis-blog-dashboard into main

---
+++ src/components/Sidebar/UserInfo.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  isSidebarOpen: boolean;
+}
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="flex align-middle h-[48px]">
+    <div
+      id="user-avatar"
+      class="w-[48px] h-[48px] bg-gray-100 justify-center rounded-full"
+    ></div>
+    <div
+      v-if="isSidebarOpen"
+      class="flex flex-col h-full align-middle justify-center"
+    >
+      WELCOME
+    </div>
+    <!-- <div id="settings">SETTINGS</div> -->
+  </div>
+</template>
+
+<style scoped></style>
