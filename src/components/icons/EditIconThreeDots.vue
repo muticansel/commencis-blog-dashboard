fe139438be31d3f5d4f5fdea805e692fee7b200c
discretetimefourier <44666921+rokasta12@users.noreply.github.com>
2022-04-08 18:49:29 +0300

    add: threedots icon added

---
+++ src/components/icons/EditIconThreeDots.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    width="4"
+    height="20"
+    viewBox="0 0 4 20"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="2.00008" cy="2.66666" r="1.83333" fill="currentColor" />
+    <circle cx="2.00008" cy="10" r="1.83333" fill="currentColor" />
+    <circle cx="2.00008" cy="17.3333" r="1.83333" fill="currentColor" />
+  </svg>
+</template>
