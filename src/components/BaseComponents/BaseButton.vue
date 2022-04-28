093049cbc322e623cfcee2e2440f02a166d978dc
muti.cansel <cancel.muti@gmail.com>
2022-04-28 12:55:38 +0200

    refactor: BaseButton lint refactor

--- src/components/BaseComponents/BaseButton.vue
+++ src/components/BaseComponents/BaseButton.vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
 interface Props {
-  text: string;
-  buttonClass?: string;
+  text: string
+  buttonClass?: string
 }
-const props = defineProps<Props>();
-const emit = defineEmits(["click"]);
+const props = defineProps<Props>()
+const emit = defineEmits(['click'])
 
 const clickHandler = () => {
-  emit("click", { name: "clicked", props: { ...props } });
-};
+  emit('click', { name: 'clicked', props: { ...props } })
+}
 </script>
 
 <template>
-  <button
-    :class="['bg-blue-500', 'text-white', 'px-5', 'py-2', buttonClass]"
-    @click="clickHandler"
-  >
+  <button :class="['bg-blue-500', 'text-white', 'px-5', 'py-2', buttonClass]" @click="clickHandler">
     {{ text }}
   </button>
 </template>
